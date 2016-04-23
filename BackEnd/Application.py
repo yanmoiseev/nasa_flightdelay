8bb50ba25f7a57cc60d88d8c59dbe65a3497c5b3
acceptable <andi.winata@hotmail.com>
2016-04-23 17:15:48 +1000

    Update Application.py

--- BackEnd/Application.py
+++ BackEnd/Application.py
@@ -1,7 +1,7 @@
 ''' provides an API for UI to access and display final result '''
-from flask import Flask,request,url_for,jsonify, flash
-from flask_restful import Api,Resource,reqparse
-import os,requests,json,xmltodict,geocoder
+from flask import Flask, request, url_for, jsonify
+from flask_restful import Api, Resource, reqparse
+import os, requests, json, xmltodict, geocoder
 from collections import OrderedDict
 
 app = Flask(__name__)
@@ -10,37 +10,34 @@ api = Api(app)
 ################################
 #### to send output to ui ######
 
-prob = os.path.join("toui.txt") # info to ui
-outstr=''
-
-def readData():
-    with open (prob,'r') as fd:
-        outstr = fd.read()
-    return outstr
-    
-l = [{'result':readData()}]
+# prob = os.path.join("toui.txt")  # info to ui
+# outstr = ''
+#
+# def readData():
+#     with open(prob, 'r') as fd:
+#         outstr = fd.read()
+#     return outstr
+#
+# l = [{'result': readData()}]
 
 def latlng(place): #convert text location to latitude and longitude
     location = geocoder.google("source") # location
     latitude = location.lat
     longitude = location.lng
-    return (latitude,longitude) 
-    
+    return (latitude,longitude)
 
 #### get and post handler to send data to UI ####
 
-@app.route('/ToUI',methods=['GET','POST']) # POSt, GET handlers to send Data 
+@app.route('/ui', methods=['GET', 'POST'])  # replying to POST,GET
 def sendDataToUI():
-    return jsonify({'data':l})
+    return jsonify({'data': 'asdsa'})
 
 source_key ='source'
 destination_key = 'destination'
 flightno_key = 'flightno'
 time_key = 'time'
 
-### to fetch source and destination info from ui.
-
-@app.route('/Find',methods=['GET','POST']) 
+@app.route('/find', methods=['GET', 'POST'])  # to accquire source and destination info
 def getDetails():
     if request.method == 'POST':
         source = request.form[source_key]
@@ -52,55 +49,99 @@ def getDetails():
         source = request.args.get(source_key)
         destination = request.args.get(destination_key)
         flightno = request.args.get(flightno_key)
-        time = request.args.get(time_key)    
-    l=[{'source':source,'destination':destination,'flightno':flightno,'time':time}]
-    return jsonify({"response":l})
+        time = request.args.get(time_key)
+    l = [{'source': source, 'destination': destination, 'flightno': flightno, 'time': time}]
+    return jsonify({"response": l})
+
 
 ####################################
 ##### make other data available ####
 
-#create list of dictionaries
-ml =[{}]
+# create list of dictionaries
+ml = [{}]
+
 
 ##### handlers for get and put from other endpoints####
-@app.route('/MI',methods=['GET','POST']) 
+@app.route('/ml', methods=['GET', 'POST'])
 def sendDataToML():
-    return jsonify({'data':ml})
+    return jsonify({'data': ml})
+
 
 ####################################
 ### to fetch data from external sources ###
 
-#def getData():
-#	url_list=[]
-#	req = requests.get("http://api.openweathermap.org/data/2.5/weather?zip=94040,us&APPID=75b19517f102504815a3449ebbbf979")
-#    #api.openweathermap.org/data/2.5/forecat/city?id=12345&APPID=75b19517f102504815a3449ebbbf979
-#    res = requests.get("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=75b19517f1f02504815a3449ebbbf979")
-#    return res.content
-    
-    
-def xgetData():
-	#req =  requests.get("http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php?whichClient=NDFDgen&lat=38.99&lon=-77.01&listLatLon=&lat1=&lon1=&lat2=&lon2=&resolutionSub=&listLat1=&listLon1=&listLat2=&listLon2=&resolutionList=&endPoint1Lat=&endPoint1Lon=&endPoint2Lat=&endPoint2Lon=&listEndPoint1Lat=&listEndPoint1Lon=&listEndPoint2Lat=&listEndPoint2Lon=&zipCodeList=&listZipCodeList=&centerPointLat=&centerPointLon=&distanceLat=&distanceLon=&resolutionSquare=&listCenterPointLat=&listCenterPointLon=&listDistanceLat=&listDistanceLon=&listResolutionSquare=&citiesLevel=&listCitiesLevel=&sector=&gmlListLatLon=&featureType=&requestedTime=&startTime=&endTime=&compType=&propertyName=&product=time-series&begin=2004-01-01T00%3A00%3A00&end=2020-04-23T00%3A00%3A00&Unit=e&maxt=maxt&Submit=Submit")
-	#re = requests.get("http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php?whichClient=NDFDgen&lat=38.99&lon=-77.01&listLatLon=&lat1=&lon1=&lat2=&lon2=&resolutionSub=&listLat1=&listLon1=&listLat2=&listLon2=&resolutionList=&endPoint1Lat=&endPoint1Lon=&endPoint2Lat=&endPoint2Lon=&listEndPoint1Lat=&listEndPoint1Lon=&listEndPoint2Lat=&listEndPoint2Lon=&zipCodeList=&listZipCodeList=&centerPointLat=&centerPointLon=&distanceLat=&distanceLon=&resolutionSquare=&listCenterPointLat=&listCenterPointLon=&listDistanceLat=&listDistanceLon=&listResolutionSquare=&citiesLevel=&listCitiesLevel=&sector=&gmlListLatLon=&featureType=&requestedTime=&startTime=&endTime=&compType=&propertyName=&product=time-series&begin=2004-01-01T00%3A00%3A00&end=2020-04-23T00%3A00%3A00&Unit=e&maxt=maxt&Submit=Submit")
-	re = requests.get("http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php?whichClient=NDFDgen&lat=38.99&lon=-77.01&listLatLon=&lat1=&lon1=&lat2=&lon2=&resolutionSub=&listLat1=&listLon1=&listLat2=&listLon2=&resolutionList=&endPoint1Lat=&endPoint1Lon=&endPoint2Lat=&endPoint2Lon=&listEndPoint1Lat=&listEndPoint1Lon=&listEndPoint2Lat=&listEndPoint2Lon=&zipCodeList=&listZipCodeList=&centerPointLat=&centerPointLon=&distanceLat=&distanceLon=&resolutionSquare=&listCenterPointLat=&listCenterPointLon=&listDistanceLat=&listDistanceLon=&listResolutionSquare=&citiesLevel=&listCitiesLevel=&sector=&gmlListLatLon=&featureType=&requestedTime=&startTime=&endTime=&compType=&propertyName=&product=time-series&begin=2016-04-25T00%3A00%3A00&end=2016-04-25T00%3A10%3A00&Unit=m&temp=temp&dew=dew&wspd=wspd&wdir=wdir&sky=sky&wx=wx&rh=rh&appt=appt&precipa_r=precipa_r&Submit=Submit")
-	xdata = xmltodict.parse(re.content)
-	#print xdata
-	xdatad= {}
-	xdatad['location'] = (xdata['dwml']['data']['location']['point']['@latitude'],xdata['dwml']['data']['location']['point']['@longitude'])
-	#xdatad['moreWeatherInformation'] = xdata['dwml']['data']['moreWeatherInformation']
-	xdatad['time-layout'] = xdata['dwml']['data']['time-layout']['start-valid-time']
-	xdatad['temp'] = xdata['dwml']['data']['parameters']
-	#print xdatad['temp']
-	xdatad['temp'] = xdata['dwml']['data']['parameters']['temperature'][0]['value']
-	xdatad['dew_temp'] = xdata['dwml']['data']['parameters']['temperature'][1]['value']
-	xdatad['wind-speed'] = xdata['dwml']['data']['parameters']['wind-speed']['value']	
-	xdatad['cloud-amount'] = xdata['dwml']['data']['parameters']['cloud-amount']['value']
-	xdatad['humidity'] = xdata['dwml']['data']['parameters']['humidity']['value']
+# this one is forming url to get weather data from http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php?
+def get_forecast_weather(lat=None, lon=None,
+                         begin=None, end=None):
 
-	return xdatad
-	
-print latlng("CPH airport")
+    base_url = 'http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php'
+    params = {}
+    params['whichClient'] = 'NDFDgen'
+    params['lat'] = lat
+    params['lon'] = lon
+    params['product'] = 'time-series'
+    params['begin'] = begin
+    params['end'] = end
+    params['Unit'] = 'm'
+    params['temp'] = 'temp'
+    params['snow'] = 'snow'
+    params['dew'] = 'dew'
+    params['wspd'] = 'wspd'
+    params['wdir'] = 'wdir'
+    params['sky'] = 'sky'
+    params['wx'] = 'wx'
+    params['ptornado'] = 'ptornado'
+    params['phail'] = 'phail'
+    params['pxtornado'] = 'pxtornado'
+    params['pxhail'] = 'pxhail'
+    params['ptotsvrtstm'] = 'ptotsvrtstm'
+    params['pxtotsvrtstm'] = 'pxtotsvrtstm'
+    params['wwa'] = 'wwa'
+    params['wgust'] = 'wgust'
+    params['maxrh'] = 'maxrh'
+    params['Submit'] = 'Submit'
+
+    # call the url, and gives request to function to process xml result
+    return requests.get(base_url, params=params)
+
+
+def xgetData(re):
+    response_dict = xmltodict.parse(re.content)
+    response_data = response_dict['dwml']['data']
+    response_params = response_data['parameters']
+    parsed_result = {}
+
+    lat = response_data['location']['point']['@latitude']
+    long = response_data['location']['point']['@longitude']
+    parsed_result['location'] = (lat, long)
+
+    def add_key_value(ordered_dict, target_dict):
+        if 'name' in ordered_dict and 'value' in ordered_dict:
+            target_dict[ordered_dict['name']] = ordered_dict['value']
+
+    def loop_dict_list(list, target_dict):
+        for l in list:
+            if isinstance(l, OrderedDict):
+                add_key_value(l, target_dict)
+            else:
+                loop_dict_list(l, target_dict)
+
+    loop_dict_list(response_params, parsed_result)
+
+    temp = response_params['temperature']
+    dew_point = response_params['']
+
+    parsed_result['location'] = (lat, long)
+    parsed_result['time-layout'] = response_data['time-layout']['start-valid-time']
+    parsed_result['temp'] = response_params
+    parsed_result['temp'] = response_params['temperature'][0]['value']
+    parsed_result['dew_temp'] = response_params['temperature'][1]['value']
+    parsed_result['wind-speed'] = response_params['wind-speed']['value']
+    parsed_result['cloud-amount'] = response_params['cloud-amount']['value']
+    parsed_result['humidity'] = response_params['humidity']['value']
+
+
+    return parsed_result
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-        
