8efdaac535600f899a8251589af2f4884f0c6d87
Andi <andi.winata@hotmail.com>
2016-04-24 10:18:42 +1000

    Basic info will be sent to ML

--- BackEnd/Application.py
+++ BackEnd/Application.py
@@ -1,24 +1,27 @@
-from flask import Flask, request, url_for, jsonify, render_template, redirect,flash
+from flask import Flask, request, url_for, jsonify, render_template, redirect, flash
 from flask_restful import Api
-import requests, xmltodict, geocoder, pytz, geopy,pickle
+import requests, xmltodict, geocoder, pytz, geopy, pickle
 from MI import calculate
 from dateutil import parser
 import datetime
 from collections import OrderedDict
 
-#pickle_file = open("lingreg.p",'rb')
-#rreg = pickle.load(pickle_file)
+# pickle_file = open("lingreg.p",'rb')
+# rreg = pickle.load(pickle_file)
 
 app = Flask(__name__)
 api = Api(app)
-#app.secret_key = 'something'
 
-#region utility
+
+# app.secret_key = 'something'
+
+# region utility
 def latlng(place):  # convert text location to latitude and longitude
     location = geocoder.google(place)  # location
     latitude = location.lat
     longitude = location.lng
     return (latitude, longitude)
+
 
 def parse_date_time_utc(date_str):
     dt = parser.parse(date_str)
@@ -29,11 +32,13 @@ def parse_date_time_utc(date_str):
 
     return dt
 
-def get_timezone(place):
-   g = geopy.geocoders.GoogleV3()
-   return g.timezone(geocoder.google(place).latlng)
 
-#end region
+def get_timezone(place):
+    g = geopy.geocoders.GoogleV3()
+    return g.timezone(geocoder.google(place).latlng)
+
+
+# end region
 
 source_key = 'origin'
 destination_key = 'destination'
@@ -44,23 +49,24 @@ arvdate_key = 'arrivaldate'
 depttime_key = 'departuretime'
 arvtime_key = 'arrivaltime'
 
+
 @app.route('/', methods=['GET'])
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/find', methods=['GET', 'POST'])  # to accquire source and destination info
-@app.route('/find/<probability>') # to display final delay probability
+@app.route('/find/<probability>')  # to display final delay probability
 def find():
     if request.method == 'POST':
-        source = request.form.get(source_key,None)
-        destination = request.form.get(destination_key,None)
-        flightno = request.form.get(flightno_key,None)
-        airline = request.form.get(airline_key,None)
-        departdate = request.form.get(deptdate_key,None)
-        arrivaldate = request.form.get(arvdate_key,None)
-        departtime = request.form.get(depttime_key,None)
-        arrivetime = request.form.get(arvtime_key,None)
+        source = request.form.get(source_key, None)
+        destination = request.form.get(destination_key, None)
+        flightno = request.form.get(flightno_key, None)
+        airline = request.form.get(airline_key, None)
+        departdate = request.form.get(deptdate_key, None)
+        arrivaldate = request.form.get(arvdate_key, None)
+        departtime = request.form.get(depttime_key, None)
+        arrivetime = request.form.get(arvtime_key, None)
     else:
         return redirect(url_for('index'))
 
@@ -71,6 +77,8 @@ def find():
 
     departure_begin = parse_date_time_utc(departure_full_time)
     departure_end = departure_begin + datetime.timedelta(seconds=1)
+    dt = departure_begin
+    print dt
     departure_begin = departure_begin.isoformat().split('+')[0]
     departure_end = departure_end.isoformat().split('+')[0]
 
@@ -79,13 +87,14 @@ def find():
     arrival_begin = arrival_begin.isoformat().split('+')[0]
     arrival_end = arrival_end.isoformat().split('+')[0]
 
-    departure_result = get_forecast_weather(source_coordinate[0], source_coordinate[1], departure_begin, departure_end)
-    #probability = rreg.predict([])
+    departure_result = get_forecast_weather(source_coordinate[0], source_coordinate[1], departure_begin, departure_end,
+                                            dt)
+    # probability = rreg.predict([])
     probability = calculate(departure_result)
-    return render_template('prediction.html',probability=probability)
+    return render_template('prediction.html', probability=probability)
 
-    #arrival_result = get_forecast_weather(dest_coordinate[0], dest_coordinate[1], arrival_begin, arrival_end)
-    #return jsonify(departure_result)
+    # arrival_result = get_forecast_weather(dest_coordinate[0], dest_coordinate[1], arrival_begin, arrival_end)
+    # return jsonify(departure_result)
 
 
 ####################################
@@ -94,7 +103,7 @@ def find():
 
 # this one is forming url to get weather data from http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php?
 def get_forecast_weather(lat=None, lon=None,
-                         begin=None, end=None):
+                         begin=None, end=None, datetime=None):
     base_url = 'http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php'
     params = {}
 
@@ -122,38 +131,44 @@ def get_forecast_weather(lat=None, lon=None,
     params['wgust'] = 'wgust'
     params['maxrh'] = 'maxrh'
     params['Submit'] = 'Submit'
-    return parse_xml_to_flat_dict(requests.get(base_url,params=params))
+    return parse_xml_to_flat_dict(requests.get(base_url, params=params), datetime)
 
-def parse_xml_to_flat_dict(request):
-    #print request.url
+
+def parse_xml_to_flat_dict(request, datetime):
+    # print request.url
     response_dict = xmltodict.parse(request.content)
-    #print request.content
+    # print request.content
     try:
         response_data = response_dict['dwml']['data']
         response_params = response_data['parameters']
     except KeyError:
-        response_data={}
+        response_data = {}
 
     parsed_result = {}
-    parsed_list = ['location','temperature','dew_point','wind_speed','wind_direction','cloud_cover_amount','snow_amount','probability_severe_thunderstorm','probability_extreme_severe_thunderstrom','wind_gust','humidity']
+    parsed_list = ['location', 'temperature', 'dew_point', 'wind_speed', 'wind_direction', 'cloud_cover_amount',
+                   'snow_amount', 'probability_severe_thunderstorm', 'probability_extreme_severe_thunderstrom',
+                   'wind_gust', 'humidity']
     for para in parsed_list:
-        parsed_result.setdefault(para,0)
+        parsed_result.setdefault(para, 0)
+
+    parsed_result['day'] = datetime.weekday()
+
     try:
-    	lat = response_data['location']['point']['@latitude']
-    	long = response_data['location']['point']['@longitude']
-    	temperature = response_params['temperature'][0]['value']
-    	dew_point = response_params['temperature'][1]['value']
-    	wind_speed = response_params['wind-speed'][0]['value']
-    	wind_direction = response_params['direction']['value']
-    	cloud_cover_amount = response_params['cloud-amount']['value']
-    	snow_amount = response_params['precipitation']['value']
+        lat = response_data['location']['point']['@latitude']
+        long = response_data['location']['point']['@longitude']
+        temperature = response_params['temperature'][0]['value']
+        dew_point = response_params['temperature'][1]['value']
+        wind_speed = response_params['wind-speed'][0]['value']
+        wind_direction = response_params['direction']['value']
+        cloud_cover_amount = response_params['cloud-amount']['value']
+        snow_amount = response_params['precipitation']['value']
 
-    	probability_severe_thunderstorm = response_params['convective-hazard'][0]['severe-component']['value']
-    	probability_extreme_severe_thunderstorm = response_params['convective-hazard'][1]['severe-component']['value']
+        probability_severe_thunderstorm = response_params['convective-hazard'][0]['severe-component']['value']
+        probability_extreme_severe_thunderstorm = response_params['convective-hazard'][1]['severe-component']['value']
 
-    	wind_gust = response_params['wind-speed'][1]['value']
-    	humidity = response_params['humidity']['value']
-   
+        wind_gust = response_params['wind-speed'][1]['value']
+        humidity = response_params['humidity']['value']
+
         parsed_result['location_lat'] = lat
         parsed_result['location_long'] = long
         parsed_result['temperature'] = temperature
@@ -168,8 +183,8 @@ def parse_xml_to_flat_dict(request):
         parsed_result['wind_gust'] = wind_gust
         parsed_result['humidity'] = humidity
     except KeyError:
-	print parsed_result
-        pass # the default values set will be passed
+        print parsed_result
+        pass  # the default values set will be passed
 
     return parsed_result
 
--- BackEnd/MI.py
+++ BackEnd/MI.py
@@ -1,4 +1,24 @@
 ### place holder for MI input and output####
 
-def calculate(d): # d is the input dictionary
-    return "Live Long and prosper"
+def calculate(d):  # d is the input dictionary
+    ml_list = []
+    ml_list.extend(convert_cloud(d['cloud_cover_amount']))
+    ml_list.append(round(int(d['dew_point']) / 5 * 9 + 32) * 10)
+    ml_list.append(round(int(d['temperature']) / 5 * 9 + 32) * 10)
+    ml_list.append(int(d['wind_speed']) * 2.23694)
+    ml_list.append(int(d['wind_direction']))
+    # TODO
+    ml_list.append(int(d['day']) + 1)
+
+    return ml_list
+
+
+def convert_cloud(cover_amount):
+    # broken, clear, few, overcast, scattered
+    clear = 100 if cover_amount == 0 else 0
+    few = 100 if cover_amount == 1 else 0
+    overcast = 100 if cover_amount == 8 else 0
+    scatter = (cover_amount - 1 % 3) * 100 if cover_amount in range(2, 5) else 0
+    broken = (cover_amount - 1 % 3) * 100 if cover_amount in range(5, 8) else 0
+
+    return (broken, clear, few, overcast, scatter)
